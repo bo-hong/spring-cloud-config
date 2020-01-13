83839e8d6fd0e41391f72e76e74b0552f379f795
hongbo <59234466@qq.com>
2020-01-13 15:22:17 +0800

    init

---
+++ order/src/main/java/com/hong/order/controller/testController.java
@@ -0,0 +1,22 @@
+package com.hong.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RefreshScope
+public class testController {
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/env")
+    public String printEnv() {
+        return env;
+    }
+
+}
